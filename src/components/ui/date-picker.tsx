43d83d208badcf9482d42b61e7b29ddc422616c0
matheusferreiradeoliveira <matheusferreiradeoliveira21@gmail.com>
2025-03-14 16:48:54 -0300

    Add new shadcn components

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { format } from "date-fns";
+import { parse, format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
@@ -13,33 +13,54 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-export function DatePicker({ placeholder }: { placeholder?: string }) {
-  const [date, setDate] = React.useState<Date>();
+export function DatePicker({
+  placeholder,
+  value,
+  setValue,
+  initialValue,
+}: {
+  placeholder?: string;
+  value: Date | null;
+  setValue: (date?: Date) => void;
+  initialValue?: Date;
+}) {
+  // const [date, setDate] = React.useState<Date>();
 
   return (
     <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn(
-            "w-[280px] justify-start text-left font-normal",
-            !date && "text-muted-foreground"
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? (
-            format(date, "PPP")
-          ) : (
-            <span>{placeholder || "Pick a date"}</span>
-          )}
-        </Button>
-      </PopoverTrigger>
+      <div className="relative">
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "w-[280px] justify-start text-left font-normal",
+              !value && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {value ? (
+              format(value, "PPP")
+            ) : (
+              <span>{placeholder || "Pick a date"}</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        {value && (
+          <a
+            onClick={() => setValue(undefined)}
+            className="text-red-500 absolute right-4 top-1"
+          >
+            x
+          </a>
+        )}
+      </div>
       <PopoverContent className="w-auto p-0">
         <Calendar
           mode="single"
-          selected={date}
-          onSelect={setDate}
+          selected={value || undefined}
+          onSelect={setValue}
           initialFocus
+          fromDate={initialValue}
         />
       </PopoverContent>
     </Popover>
