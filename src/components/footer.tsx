94ba805f65f7bdf28560175b680c4698c611a206
MatheusFerreiradeOliveira21 <matheusferreiradeoliveira21@gmail.com>
2025-03-16 16:17:43 -0300

    finish footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,32 +6,36 @@ const apis = [
     name: "News API",
     url: "https://newsapi.org/",
   },
+  {
+    name: "The Guardian API",
+    url: "https://open-platform.theguardian.com/documentation/",
+  },
+  {
+    name: "The New York Times API",
+    url: "https://developer.nytimes.com/docs/articlesearch-product/1/overview",
+  },
 ];
 
 export default function Footer() {
   return (
-    <div className="text-foreground order-3 bg-background border-t-[2px] border-t-slate-200 min-h-footerHeight flex flex-col md:flex-row justify-between items-center w-full text-gray-200 px-10">
-      <div className="">
+    <div className="text-foreground order-3 bg-background border-t-[2px] border-t-slate-200 min-h-footerHeight flex flex-col md:flex-row justify-between items-center w-full px-10">
+      <div className="text-lg">
         <b>© Innoscripta</b>
       </div>
-      {/* <div className="flex flex-row items-center gap-10">
-        <p className="text-sm ">Public APIs:</p>
+      <div className="flex flex-row items-center gap-10">
+        <p className="text-lg">Public APIs:</p>
         <div className="flex flex-row items-center gap-4">
-          <div className="bg-blue-600 white tracking-tighter px-[5px] pt-[4px] pb-[4px]">
-            News
-            <span className="bg-white text-blue-600 px-[0.2rem] mx-[0.2rem] mt-[3px] mb-[8px]">
-              API
-            </span>
-          </div>
-
-          <Image
-            width={180}
-            height={180}
-            alt="The Guardian"
-            src={"/images/theguardian-op-logo.svg"}
-          />
+          {apis.map((api) => (
+            <a
+              target="blank"
+              className="cursor-pointer p-2 rounded-sm bg-foreground text-background "
+              href={api.url}
+            >
+              {api.name}
+            </a>
+          ))}
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
